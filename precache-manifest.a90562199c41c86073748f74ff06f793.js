2062955f5a3da1355b90c7c94ce64ad7c3db9349
sherry-chan727 <sherry-chan727@users.noreply.github.com>
2020-07-23 08:19:11 +0000

    deploy: a4209090f3261f7bcc7dbcfbaf93103e9b0b818b

---
+++ precache-manifest.a90562199c41c86073748f74ff06f793.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c1e3df2977bd5a72553d393fd3d58c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a417f01269795b6998f4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "35877c64d671120974a5",
+    "url": "/static/js/2.cca4ac28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cca4ac28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a417f01269795b6998f4",
+    "url": "/static/js/main.0ca5063b.chunk.js"
+  },
+  {
+    "revision": "f8e29083eae59e34a720",
+    "url": "/static/js/runtime-main.caf10fbe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
